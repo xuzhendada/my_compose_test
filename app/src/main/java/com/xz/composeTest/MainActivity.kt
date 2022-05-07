b57fa81fb7a20941e595fa8a583531d921c7d9d2
xuzhen <xuzhendada@sina.com>
2022-05-07 20:36:50 +0800

    first commit

---
+++ app/src/main/java/com/xz/composeTest/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.xz.composeTest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.xz.composeTest.ui.theme.My_compose_testTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            My_compose_testTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = false)
+@Composable
+fun DefaultPreview() {
+    My_compose_testTheme {
+        Greeting("Android")
+    }
+}
